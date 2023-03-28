b0b7a071fa663382f45be08100bb533f86304dc2
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-03-28 16:25:34 -0300

    Create jogo.js

---
+++ jogo.js
@@ -0,0 +1,17 @@
+var altura = 0;
+var largura = 0;
+
+function ajustaTamanhoTelaJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+
+    console.log(altura, largura);
+}
+
+ajustaTamanhoTelaJogo();
+
+
+var posicaoX = Math.random();
+var posicaoY = Math.random();
+
+console.log(posicaoX, posicaoY)
